b11b8c89a29800c4b6b5d276eeefb3ad891da3f8
Andrey-V-Georgiev <andrey.v.georgiev@gmail.com>
2020-11-22 19:18:30 +0200

    _05_EXERCISE_XML_PROCESSING initial

---
+++ _05_EXERCISE_XML_PROCESSING/src/test/java/spring_data/car_dealer/CarDealerApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_data.car_dealer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarDealerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
